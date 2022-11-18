751b5b6cfae0104e74ec380029f1260ca9e813b4
kati1989 <kati_1989@yahoo.com>
2022-11-18 23:50:03 +0200

    added  method create buttons

--- functions.js
+++ functions.js
@@ -39,3 +39,36 @@ function attachCards(arr){
     }
 }
 
+function createButton(){
+    let div=document.createElement("button");
+    div.classList.add('buttons');
+
+    let btn=document.createElement("button");
+    btn.classList.add('button');
+    btn.textContent=[];
+    div.appendChild(btn)
+
+    return div;
+
+
+}
+
+//o functie ce primeste ca parametru un numar si afiseaza atatea butoane cate am dat in parametru
+function createButtons(nr){
+    let container=document.querySelector(".buttons")
+   
+    for(let i=1;i<=nr;i++){
+        const element=createButton();
+        element.textContent=i;
+        container.appendChild(element);
+    }
+}
+
+
+// function pages(arr,page){
+//     let arrNou=[];
+//     for(let i=0+page*9;i<page*9;i++){
+//         arrNou.push
+//     }
+
+// }
