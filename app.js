0f079fd819c3fea42bd87e836ac3997d1ce69777
kati1989 <kati_1989@yahoo.com>
2022-11-24 20:04:35 +0200

    apare persaoana infata pe care am dat click

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 load();
+//addModal();
 
+
+let employeeContainer=document.querySelector('.container');
+
+employeeContainer.addEventListener("click",(e)=>{
+    let employee=e.target;
+
+    addModal(employee);
+});
