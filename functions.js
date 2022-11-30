9678c0319cd85cfea8e19910a3ef61f173315cea
kati1989 <kati_1989@yahoo.com>
2022-11-30 19:44:20 +0200

    Delete button

--- functions.js
+++ functions.js
@@ -23,7 +23,24 @@ function createCard(obj){
 
     let data=document.createElement("footer");
     data.classList.add('data');
-    data.textContent=obj.data;
+    data.textContent=obj.registered.date;
+
+   
+    div.appendChild(data);
+    
+    let div4=document.createElement('div');
+    div.classList.add('delete-container')
+
+    let btnDelete=document.createElement("button");
+    btnDelete.classList.add("delete");
+ 
+    btnDelete.textContent="Delete";
+
+
+
+    div4.appendChild(btnDelete);
+
+    div.appendChild(div4);
 
     return div;
 }
@@ -89,36 +106,138 @@ function load(){
 }
 
 
-function returnObiect(arr,name){
-    for(let i=0;i<arr.length;i++){
-        if(arr[i].name.first==name){
-            return arr[i];
-        }
-    }
+//append
+function createCardModal(obj){
 
-    return -1;
-}
+    let div=document.createElement('div');
+    div.classList.add('modal-card');
 
-function cretaeCardModal(){
-    let div=document.createElement("div");
-    div.classList.add('employee');
+    let div2=document.createElement('div');
+    div2.classList.add('x');
 
-    let imagine=document.createElement('img');
-    imagine.classList.add('image');
-    div.appendChild(imagine);
 
-    let  nume=document.createElement('h1');
-    nume.classList.add('name');
-    div.appendChild(nume);
+    let btnX=document.createElement('button');
+    btnX.classList.add('btnX');
 
-    let email=document.createElement('p');
-    email.classList.add('email');
-    div.appendChild()
+    btnX.textContent="x";
+
+
+    btnX.addEventListener("click",()=>{
+
+        let modal=document.querySelector(".modal");
+
+        modal.innerHTML="";
+
+        modal.classList.add("hide");
     
 
+
+    })
+
+    div2.appendChild(btnX);
+
+
+    div.append(div2);
+
+
+    let imagine=document.createElement('img');
+    imagine.src=obj.picture.medium;
+
+    div.appendChild(imagine);
+
+    let div3=document.createElement('div');
+    div3.classList.add('directii');
+
+    div3.innerHTML=`
+    <button class="left"><i class="fa-solid fa-caret-left"></i></button>
+    <button class="right"><i class="fa-solid fa-caret-right"></i></button>
+    `
+
+    let btnLeft=div3.querySelector(".left");
+   
+    btnLeft.addEventListener("click",()=>{
+     
+        //createCardModal(returnPrevious(data,obj.name.first));
+
+       
+        let t=returnPrevious(obj.name.first);
+
+        let card=createCardModal(t);
+        let modal=document.querySelector(".modal");
+
+        modal.innerHTML="";
+
+        modal.appendChild(card);
+
+       
+    })
+
+    let btnRight=div3.querySelector('.right');
+
+    btnRight.addEventListener("click",()=>{
+        let t=returnNext(obj.name.first);
+
+        let card=createCardModal(t);
+        let modal=document.querySelector(".modal");
+
+        modal.innerHTML="";
+        modal.appendChild(card);
+    })
+
+    div.appendChild(div3);
+    
+    let name=document.createElement('h1');
+ 
+
+    name.textContent=obj.name.first+" "+obj.name.last;
+   
+    div.appendChild(name);
+
+    let email=document.createElement('p');
+    email.textContent=obj.email;
+
+    div.appendChild(email);
+
+    let data=document.createElement('p');
+    data.textContent=obj.registered.date;
+    div.appendChild(data);
+
+
+
+  
     return div;
 
 }
+
+function returnPrevious(name){
+    for(let i=1;i<data.length;i++){
+        if(data[i].name.first==name){
+            return data[i-1];
+        }
+    }
+    return -1;
+}
+
+function returnNext(name){
+    for(let i=0;i<data.length-1;i++){
+        if(data[i].name.first==name){
+            return data[i+1];
+        }
+    }
+    return -1;
+}
+
+
+function attachCards(arr){
+    let container=document.querySelector(".container")
+    for(let i=0;i<arr.length;i++){
+        container.appendChild(createCard(arr[i]));
+    }
+}
+
+
+
+
 function addModal(obj){
     let container=document.querySelector(".modal");
     
@@ -127,3 +246,53 @@ function addModal(obj){
     container.appendChild(obj);
 
 }
+
+//returneaza obiectul dupa nume si prenume
+//Ficat Kati
+function returnObj(arr,name){
+    for(let i=0;i<arr.length;i++){
+
+        let numePers=arr[i].name.first+" "+arr[i].name.last
+
+        console.log(numePers);
+        console.log(name);
+        if(numePers==name)
+               return arr[i];
+    }
+
+    return -1;
+}
+
+let listContainer=document.querySelector(".container");
+let btnDelete=document.createElement("button");
+listContainer.addEventListener("click",(e)=>{
+    let ob=e.target;
+
+    if(ob.classList.contains("delete")){
+        let div=ob.parentNode.parentNode;
+        listContainer.removeChild(div);
+    }
+})
+
+
+/*
+let btnAdd=document.querySelector('.add');
+let inp=document.getElementById('element');
+let inp2=document.getElementById('elementPrice');
+let listContainer=document.querySelector(".container");
+
+
+
+let btnDelete=document.createElement("button");
+
+listContainer.addEventListener("click",(e)=>{
+    let ob=e.target;
+
+    if(ob.classList.contains("delete")){
+        let div=ob.parentNode.parentNode;
+        listContainer.removeChild(div);
+        
+    }
+})*/
+
+//cand apasam pe x sa incida modalul
